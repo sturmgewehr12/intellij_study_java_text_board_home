f013ea134ba6b763e635e5e648e8dfd58d9eb28e
KimdoHyeon <gimd09944@gmail.com>
2023-10-09 02:20:46 +0900

    명령어 받고 출력

--- src/com/sbs/java/kdh/board/Main.java
+++ src/com/sbs/java/kdh/board/Main.java
@@ -1,12 +1,47 @@
 package com.sbs.java.kdh.board;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("== 시작 ==");
-    System.out.println("== 실행 ==");
-    System.out.println("== 종료 ==");
+    int id = 1;
+    while (true){
+
+      System.out.println("== 명령어 입력 ==");
+      String cmd = sc.nextLine();
+
+      if(cmd.equals("make")){
+        System.out.println("== 게시물 등록 ==");
+
+        System.out.print("제목 : ");
+        String cmd2 = sc.nextLine();
+
+        System.out.print("내용 : ");
+        String cmd3 = sc.nextLine();
+
+
+        System.out.printf(" %d번 게시물 작성 완료! -- 제목 : %s || 내용 : %s", id, cmd2, cmd3);
+        id++;
+      }
+      else if(cmd.equals("exit")){
+        System.out.println("프로그램을 종료 합니다.");
+        break;
+      }else{
+        System.out.println("명령어 잘못 입력");
+      }
+
+
+      System.out.printf("입력 된 명령어 : %s\n", cmd);
+
+    }
+
+   sc.close();
   }
 }

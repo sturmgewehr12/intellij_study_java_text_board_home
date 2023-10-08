21aa8f0e826d47bde4b73178e3a68bf8fc5e1ace
KimdoHyeon <gimd09944@gmail.com>
2023-10-09 04:59:44 +0900

    게시물 최근 리스트

--- src/com/sbs/java/kdh/board/Main.java
+++ src/com/sbs/java/kdh/board/Main.java
@@ -1,6 +1,7 @@
 package com.sbs.java.kdh.board;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -13,6 +14,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     Article lastArticle = null;
+    List<Article> list = new ArrayList<>();
 
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("== 시작 ==");
@@ -36,16 +38,32 @@ public class Main {
         //System.out.printf(" %d번 게시물 작성 완료! -- 제목 : %s || 내용 : %s", id, cmd2, cmd3);
         System.out.println("생성된 게시물 객체 : " + article);
 
-        id++;
-      } else if (cmd.equals("detail")) {
-        Article article = lastArticle;
+        list.add(article);
 
-        if(article == null){
+        id++;
+      } else if (cmd.equals("list")) {
+        System.out.println("게시물 리스트");
+        System.out.println("번호 / 제목");
+        /* v1
+      for(int i = 0; i < articles.size(); i++) {
+        Article article = articles.get(i);
+        System.out.printf("%d / %s\n", article.id, article.title);
+      }
+      */
+        ;
+        for(int i = list.size() - 1; i >= 0; i--) {
+          Article article = list.get(i);
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
+      } else if (cmd.equals("detail")) {
+
+        if(lastArticle == null){
           System.out.println("게시물 없음!");
           continue;
         }
         else{
-          System.out.println("최근 게시물 : " + article);
+          System.out.println("최근 게시물 : " + lastArticle);
         }
 
 
@@ -63,6 +81,8 @@ public class Main {
 
     sc.close();
   }
+
+
 
 
 }
